be906d590f4147a7fb6b87baa8531c32fce83006
Wendell <1925005015@qq.com>
2025-07-27 14:12:43 +0800

    first commit

---
+++ minioapi/src/main/java/com/wenhao/minioapi/MinioapiApplication.java
@@ -0,0 +1,13 @@
+package com.wenhao.minioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinioapiApplication.class, args);
+	}
+
+}
---
+++ minioapi/src/test/java/com/wenhao/minioapi/MinioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenhao.minioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
